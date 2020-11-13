b3ce35855add2b1df76acf83bc076bfed9c51851
Steven Pak <spak9@gmu.edu>
2020-11-13 11:33:34 -0500

    commented and refined some parts

--- basic_tweeter.py
+++ basic_tweeter.py
@@ -20,22 +20,28 @@ class Tweeter():
 
 
 def main():
-    """ A main() that can be used as a script to tweet either
-        1. new cases & total cases of a given day
-        2. total cases of the most recent updated day """
+    """ A main() that will loop every minute to listen to replies
+        and every hour to tweet basic metrics solely on 'Fairfax County' """
+
     tweeter = Tweeter()
+    # a basic counter to make sure that our tweets are all 'unique'
     i = 1
+    # an hour counter to tweet 'Fairfax County' metric every hour
+    hour_keeper = 1
+    # every tweet has a unique tweet id
+    since_id = 1
     county = 'Fairfax County'
-    # loop and do basic tweeting of the covid metrics
-    # every 2 minutes
+
+    # 1st loop is for hourly tweeting
     while True:
-        hour_keeper = 1
-        since_id = 1
+        # 2nd loop is to listen every minute for new replies
         while True:
             since_id = mention_reply(tweeter, since_id, i)
             print('since_id', since_id)
             hour_keeper += 1
+            # if we reach an hour, reset
             if (hour_keeper == 60):
+                hour_keeper = 1
                 break
             else: time.sleep(60)
             i += 1
@@ -65,14 +71,22 @@ def main():
         if (i > 1440): i = 1
 
 def mention_reply(tweeter, since_id, i):
+    """ 'mention_reply' will check our twitter account for specific replies of
+    the form: 'County_name County' and if the reply passes, reply back with
+    basic covid metric """
+
     new_since_id = since_id
     user_name = '@COVID_Updater'
     mentions = tweepy.Cursor(tweeter.api.search, q=f'to:{user_name}', since_id=since_id).items()
     # if there are no mentions, exit
     if (True):
         print('Entering mention_reply')
+        # iterate through all the replies since 'since_id'
         for tweet in mentions:
-            if (tweet.id <= since_id): return
+            # if some reply's id is less than or equal the current 'since_id',
+            # then we know that we've seen it and don't want to reply
+            if (tweet.id <= since_id): return since_id
+
             new_since_id = max(tweet.id, new_since_id)
             # the text of the tweet
             text = tweet.text
@@ -83,10 +97,16 @@ def mention_reply(tweeter, since_id, i):
             county = ' '.join([token[0].upper() + token[1:] for token in text.split()])
             print('County: ', county)
             covid_data = data.get_data(county=county)
+            today = date.today().strftime("%-m/%-d/%y")
+
+            # county doesn't exist or incorrectly formattted
             if not covid_data:
                 print('Dictionary not valid')
-                return -1
-            today = date.today().strftime("%-m/%-d/%y")
+                tweeter.api.update_status(
+                    status=f'Sorry, either county does not exist or reply was misformatted. Is it in the format: County_name County?',
+                    in_reply_to_status_id=tweet.id
+                )
+                return since_id
 
             # check if there is data for today
             if today in covid_data:
@@ -94,7 +114,7 @@ def mention_reply(tweeter, since_id, i):
                 known_cases = covid_data[today]['known_cases']
                 tweeter.api.update_status(
                     status=f'{today}: New COVID-19 Cases in {county}: {new_cases_today}\n'
-                    f'Total COVID-19 Cases in {county}: {known_cases}. {i}/24\n',
+                    f'Total COVID-19 Cases in {county}: {known_cases}. {i}/1440\n',
                     in_reply_to_status_id=tweet.id)
                 print('Tweet Successful')
             # old data
@@ -102,7 +122,7 @@ def mention_reply(tweeter, since_id, i):
                 today = list(covid_data.keys())[0]
                 known_cases = covid_data[today]['known_cases']
                 tweeter.api.update_status(
-                status=f'{today}: Total COVID-19 Cases in {county}: {known_cases}. {i}/24\n',
+                status=f'{today}: Total COVID-19 Cases in {county}: {known_cases}. {i}/1440\n',
                 in_reply_to_status_id=tweet.id)
                 print('Tweet Successful')
 
